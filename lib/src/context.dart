e71e16915f459210a9a7b30a36f72bf86d721506
Olzhas Suleimen <amajisenpai@gmail.com>
2025-09-26 14:50:08 +0500

    Remove deprecated.

--- lib/src/context.dart
+++
@@ -1,4 +0,0 @@
-@Deprecated('Use `package:jinja/src/runtime.dart` instead.')
-library;
-
-export 'package:jinja/src/runtime.dart' show Context;
--- lib/src/loop.dart
+++
@@ -1,5 +0,0 @@
-@Deprecated('Use `package:jinja/src/runtime.dart` instead')
-library;
-
-export 'package:jinja/src/runtime.dart'
-    show LoopContext, LoopIterator, Cycler, CyclerIterator;
